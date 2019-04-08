a613709ceed37472c520128ab5eee891c36ff22a
cosmas <moringa@gmail.com>
2019-04-08 08:58:46 +0300

    add script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-//user interface 
-$(document).ready(function(event)){
+//user interface
+$(document).ready(function(event){
 
-  $("button#start").click(function(event)){
+  $("button#start").click(function(event){
   player1= new Player(true);
   player2= new Player(false);
   $(".player-console").show();
@@ -15,7 +15,22 @@ $(document).ready(function(event)){
 
   player1.playerName=player1Name
   player2.playerName=player2Name
-}
+
+});
+
+$("button#New Game").click(function(event){
+  $(".player-console").hide();
+  clearValues();
+  player1.newgame();
+  player2.newgame();
+  $("#Round-total-1").empty();
+  $("#Total-scores-1").empty();
+$("#Dice-roll-1").empty();
+$("#Round-total-2").empty();
+$("#Total-score-2").empty();
+$("#Dice-roll-2").empty();
+(".start").show();
+
 });
 
 
