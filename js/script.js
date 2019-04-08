d72748ad3c3e2eb1ada615160fe2f2bb446a2bc5
cosmas <moringa@gmail.com>
2019-04-08 11:57:09 +0300

    change script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,39 @@
+
+//user interface
+$(document).ready(function(event){
+
+  $("button#start").click(function(event){
+  player1= new Player(true);
+  player2= new Player(false);
+  $(".player-console").show();
+ $(".start").hide():
+
+  var player1Name= $(".player1Name").val();
+  $("#player1Name").text(player1Name);
+
+  var player2Name= $(".player2Name").val();
+  $("#player2Name").text(player2Name);
+
+  player1.playerName=player1Name
+  player2.playerName=player2Name
+});
+
+$("button#New Game").click(function(event){
+  $(".player-console").hide();
+  clearValues();
+  player1.newgame();
+  player2.newgame();
+  $("#Round-total-1").empty();
+  $("#Total-scores-1").empty();
+$("#Die-roll-1").empty();
+$("#Round-total-2").empty();
+$("#Total-score-2").empty();
+$("#Die-roll-2").empty();
+
+(".start").show();
+
+});
+
 // business logic
 var player1="";
 var player2="";
@@ -43,38 +79,3 @@ Player.prototype.winnerCheck = function () {
     alert(this.playerName + " You are the winner!");
   }
 }
-
-//user interface
-$(document).ready(function(event){
-
-  $("button#start").click(function(event){
-  player1= new Player(true);
-  player2= new Player(false);
-  $(".player-console").show();
- $(".start").hide():
-
-  var player1Name= $(".player1Name").val();
-  $("#player1Name").text(player1Name);
-
-  var player2Name= $(".player2Name").val();
-  $("#player2Name").text(player2Name);
-
-  player1.playerName=player1Name
-  player2.playerName=player2Name
-});
-
-$("button#New Game").click(function(event){
-  $(".player-console").hide();
-  clearValues();
-  player1.newgame();
-  player2.newgame();
-  $("#Round-total-1").empty();
-  $("#Total-scores-1").empty();
-$("#Die-roll-1").empty();
-$("#Round-total-2").empty();
-$("#Total-score-2").empty();
-$("#Die-roll-2").empty();
-
-(".start").show();
-
-});
